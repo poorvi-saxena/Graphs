e79a339ef894e4435a97121c6e74c9e904e6b405
poorvi-saxena <58638025+poorvi-saxena@users.noreply.github.com>
2021-07-13 10:21:58 +0530

    Add files via upload
    Graphs using Prim's and Kruskal's algorithm

--- main.cpp
+++ main.cpp
@@ -1,196 +1,181 @@
 #include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
+#include <climits>
+#define I INT_MAX
 
 using namespace std;
 
-struct node
+// ------ PRIM'S MINIMUM COST SPANNING TREE---------
+
+// Adjacency matrix starting from (1,1)
+int cost[][8]={{I,I,I,I,I,I,I,I},
+                {I,I,25,I,I,I,5,I},
+                {I,25,I,12,I,I,I,10},
+                {I,I,12,I,8,I,I,I},
+                {I,I,I,8,I,16,I,14},
+                {I,I,I,I,16,I,20,18},
+                {I,5,I,I,I,20,I,I},
+                {I,I,10,I,14,18,I,I}};
+
+    // finds out it is connected to which vertex
+int near[8]={I,I,I,I,I,I,I,I};
+    // generates a spanning tree
+int t[2][6];
+
+void PrimsMST()
 {
-  struct node *left;
-  int data;
-  struct node *right;
-}*root=NULL;
-//root is globally accessible
+ // u,v - vertex
+    int i, j, k, u, v, n=7, mn=I;
+
+    // Step 1 - find minimum cost edge
+    for(i=1; i<=n; i++)
+    {
+        // only from upper triangular part
+        for(j=i; j<=n; j++)
+        {
+            // if cost is less than min, min=cost of i. We got an edge
+            if(cost[i][j] < mn)
+            {
+                mn=cost[i][j];
+                u=i;
+                v=j;
+            }
+        }
+    }
+    t[0][0]=u;
+    t[1][0]=v;
+    near[u]=0;
+    near[v]=0;
+
+    // initialize near array - nearer to u or v
+    for(i=1; i<=n; i++)
+    {
+        if(near[i]!=0)
+        {
+            if(cost[i][u] < cost[i][v])
+                near[i]=u;
+            else
+                near[i]=v;
+        }
+    }
+
+    for(i=1; i<n-1; i++)
+    {
+        mn=I;
+     // find which is nearer from the near array
+        for(j=1; j<=n; j++)
+        {
+            if(near[j]!=0 && cost[j][near[j]] < mn)
+            {
+                k=j;
+                mn=cost[j][near[j]];
+            }
+        }
+        t[0][i]=k;
+        t[1][i]=near[k];
+        near[k]=0;
+
+        for(j=1; j<=n; j++)
+        {
+            if(near[j]!=0 && cost[j][k] < cost[j][near[j]])
+                near[j]=k;
+        }
+    }
+    for(i=0; i<n-1; i++)
+    {
+        cout<<"("<<t[0][i]<<","<<t[1][i]<<")"<<endl;
+    }
+}
 
 
-void insert(int key)
+// ----------- KRUSKAL'S MINIMUM COST SPANNING TREE-------------
+
+int edges[3][9] = {{1,1,2,2,3,4,4,5,5},              // first vertex
+                    {2,6,3,7,4,5,7,6,7},             // second vertex
+                    {25,5,12,10,8,16,14,20,18}};     // cost of edges
+
+int set[8] = {-1,-1,-1,-1,-1,-1,-1,-1};
+int included[9] = {0};
+int tofk[2][6];          // solution where spanning tree is stored
+
+void Myunion(int u, int v)
 {
-    struct node *t=root;
-    struct node *r, *p;
-
-    if(t==NULL)
+    if(set[u] < set[v])
     {
-        p=(struct node *)malloc(sizeof(struct node));
-        p->data=key;
-        p->left=p->right=NULL;
-        root=p;
-        return;
-    }
-    while(t)
-    {
-        r=t;
-        if(t->data<key)
-        {
-            t=t->right;
-        }
-        else if(t->data>key)
-        {
-            t=t->left;
-        }
-        else
-            return;
-    }
-    p=(struct node *)malloc(sizeof(struct node));
-    p->data=key;
-    p->left=p->right=NULL;
-
-    if(key>r->data)
-    {
-        r->right=p;
+        set[u] += set[v];
+        set[v] = u;
     }
     else
-        r->left=p;
+    {
+        set[v] += set[u];
+        set[u] = v;
+    }
 }
 
-struct node * recInsert(struct node *p, int key)
+int find(int u)
 {
-    struct node *t=NULL;
-    if(p==NULL)
+    int x=u, v=0;
+    while(set[x] > 0)
     {
-        t=(struct node *)malloc(sizeof(struct node));
-        t->data=key;
-        t->right=t->left=NULL;
-        return t;
+        x = set[x];
     }
 
-    else if(p->data > key)
+    // Collapsing Find - connect found vertex to the parent
+    while(u != x)
     {
-        p->left=recInsert(p->left , key);
+        v = set[u];
+        set[u] = x;
+        u = v;
+    }
+    return x;
+}
+
+void KruskalsMST()
+{
+    int i, j, k, u, v, mn=I, n=7, e=9;
+
+    i = 0;
+    while(i < n-1)
+    {
+        mn=I;
+        for(j=0; j<e; j++)
+        {
+           if(included[j]==0 && edges[2][j] < mn)
+           {
+               mn = edges[2][j];
+               u=edges[0][j];
+               v=edges[1][j];
+               k=j;             //record minimum value
+           }
+        }
+
+        if(find(u) != find(v))
+        {
+            tofk[0][i] = u;
+            tofk[1][i] = v;
+            Myunion(find(u),find(v));           // join the parents
+            i++;
+        }
+        included[k]=1;
     }
 
-    else if(p->data < key)
+    // traverse and display
+    for(i=0; i<n-1; i++)
     {
-        p->right=recInsert(p->right, key);
-    }
-
-    return p;
-}
-
-struct node * search(int key)
-{
-    struct node *t=root;
-
-    while(t)
-    {
-        if(t->data==key)
-        {
-            return t;
-        }
-        else if(t->data>key)
-        {
-            t=t->left;
-        }
-        else if(t->data<key)
-        {
-            t=t->right;
-        }
-    }
-
-    return NULL;
-}
-
-int height(struct node *p)
-{
-    int x=0, y=0;
-    if(p==NULL)
-        return 0;
-    x=height(p->left);
-    y=height(p->right);
-    return x>y?x+1:y+1;
-}
-struct node * inSucc(struct node *p)
-{
-    while(p && p->left)
-        p=p->left;
-    return p;
-}
-
-struct node * inPre(struct node *p)
-{
-    while(p && p->right)
-        p=p->right;
-    return p;
-}
-
-struct node *del(struct node *p, int key)
-{
-    struct node *q;
-
-    if(p==NULL)
-        return NULL;
-
-    if(p->left==NULL && p->right==NULL)
-    {
-        if(p==root)
-            root=NULL;
-        free(p);
-        return NULL;
-    }
-
-    if(key > p->data)
-        del(p->right,key);
-    else if(key < p->data)
-        del(p->right,key);
-
-    else
-        if(height(p->left) > height(p->right))
-        {
-            q=inPre(p->left);
-            p->data=q->data;
-            p->left=del(p->left, q->data);
-        }
-        else
-        {
-            q=inSucc(p->right);
-            p->data=q->data;
-            p->right=del(p->right, q->data);
-        }
-    return p;
-}
-
-void Inorder(struct node *root)
-{
-    if(root)
-    {
-      Inorder(root->left);
-      cout<<root->data<<"\t";
-      Inorder(root->right);
+        cout<<"("<<tofk[0][i]<<","<<tofk[1][i]<<")"<<endl;
     }
 }
 
 int main()
 {
-    struct node *temp;
-    root=recInsert(root,10);
-    recInsert(root,5);
-    recInsert(root,30);
-    recInsert(root,20);
-    recInsert(root,45);
-    recInsert(root,80);
+    int x;
+    cout<<"1 for Prim's and 2. for Kruskal's."<<endl;
+    cin>>x;
+    cout<<endl;
 
-    del(root,10);
-
-    Inorder(root);
-    cout<<"\n";
-
-    temp=search(80);
-    if(temp!=NULL)
-        cout<<temp->data<<"  found";
+    if(x==1)
+        PrimsMST();
     else
-    {
-        cout<<"Not found in the BST";
-    }
-
-    cout<<"\n\n";
+        KruskalsMST();
+    cout<<"\n";
     return 0;
 }
